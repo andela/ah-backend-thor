c8d9f32aa25dda7f6795f888ac408d0a64e4669d
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-10-24 18:37:16 +0300

    Revert "Develop (#13)" (#15)
    This reverts commit bf08440b50306a24c9d34c86dd4cd078e078bd37.

--- authors/apps/authentication/urls.py
+++ authors/apps/authentication/urls.py
@@ -5,7 +5,13 @@ from .views import (
 )
 
 urlpatterns = [
+<<<<<<< HEAD
     path('user/', UserRetrieveUpdateAPIView.as_view(), name='get_edit_user'),
     path('users/', RegistrationAPIView.as_view(), name='create_user'),
     path('users/login/', LoginAPIView.as_view(), name='login'),
+=======
+    path('user/', UserRetrieveUpdateAPIView.as_view()),
+    path('users/', RegistrationAPIView.as_view()),
+    path('users/login/', LoginAPIView.as_view()),
+>>>>>>> Revert "Develop (#13)" (#15)
 ]
