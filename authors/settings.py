e663caf75fc8435ddb76e843142846852fac6063
cdvx <cdvx@fox.com>
2018-10-24 17:29:04 +0300

    [chore #161382384] Set up postgres in .travis.yml

--- authors/settings.py
+++ authors/settings.py
@@ -92,16 +92,16 @@ if os.environ.get('DJANGO_ENV') == 'Testing':
     
 else:
     DATABASES = {
-        'default': {
-            
+    'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ah_backend', 
-            'USER': 'postgres',
-            'PASSWORD': '',
-            'HOST': 'localhost',
+            'NAME': 'ah_backend' or 'd6c4en05qgrh43', 
+            'USER': 'postgres' or 'tmnhiwsifwhugi',
+            'PASSWORD': '' or 'f4f69ae75d63ba0ad0a26fc48c49baed2c61845ba3c8a2c95405450acb7817a8',
+            'HOST': 'localhost' or 'ec2-107-22-189-136.compute-1.amazonaws.com',
             'PORT': '5432',
         },
     }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1.2/settings/#auth-password-validators
