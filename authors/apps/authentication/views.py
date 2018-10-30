63cd75ad104a1f657779590cd545ae88f7b061e3
Joshua Mugisha <joshuamugisha@MacBook-Pro-90.local>
2018-10-30 13:05:40 +0300

    [ft #161382387] changes class-based views to generic views

--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -2,15 +2,15 @@ from rest_framework import status
 from rest_framework.generics import RetrieveUpdateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
-
+# from rest_framework.views import APIView
+from rest_framework import generics
 from .renderers import UserJSONRenderer
 from .serializers import (
     LoginSerializer, RegistrationSerializer, UserSerializer
 )
 
 
-class RegistrationAPIView(APIView):
+class RegistrationAPIView(generics.CreateAPIView):
     # Allow any user (authenticated or not) to hit this endpoint.
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
@@ -29,7 +29,7 @@ class RegistrationAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-class LoginAPIView(APIView):
+class LoginAPIView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     renderer_classes = (UserJSONRenderer,)
     serializer_class = LoginSerializer
