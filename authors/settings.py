bd518391df1d420a0db27e0fbcd8073f94035c56
johnkegz <johnkal24@gmail.com>
2018-11-05 11:36:15 +0300

    ch[#161711202]:Put password and email into environmental variables.

--- authors/settings.py
+++ authors/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'authors.apps.profiles',
     'django_nose',
     'drf_yasg',
-    'rest_framework_swagger'
+    
 
 ]
 SWAGGER_SETTINGS = {
@@ -100,7 +100,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ah_backend' or 'd8tsnl27l58vb7',
         'USER': 'postgres' or 'xroopnzwkgzrvd',
-        'PASSWORD': '' or 'kegz' or '0acbdff0e8f4398a826823d771d0e5bea0c3e510d91edfe1d7847e03405afa43',
+        'PASSWORD': '' or 'kegz' or os.getenv("HEROKU_PASSWORD"),
         'HOST': 'localhost' or 'ec2-107-20-211-10.compute-1.amazonaws.com',
         'PORT': '5432',
     },
@@ -177,8 +177,8 @@ NOSE_ARGS = [
 django_heroku.settings(locals())
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'andelateamthor@gmail.com'
-EMAIL_HOST_PASSWORD = 'team-thor-12345'
+EMAIL_HOST_USER = os.getenv("EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
