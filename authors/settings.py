a0c3f8ba1dbd5f8a14136c74d3b500a29c1baad6
Esther <esther.namusisi@andela.com>
2018-12-13 18:14:41 +0300

    sent email link to front end url

--- authors/settings.py
+++ authors/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRETE_KEY")
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
